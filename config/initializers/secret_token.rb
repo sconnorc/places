41aaccbd511bd49ca814695653fb418ac3dd9e04
Connor Cunningham <connorcunningham@wr-130-64-96-103.medford.tufts.edu>
2013-10-01 16:48:56 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Places::Application.config.secret_key_base = 'dfe2e2eb56501c3553a5add2e55b7f65c6b5413a185b660886b9c839c2fa953d3744cb80381f6f8857d43570abe3c7956a8705e9d5a13870ed18b02fbf5a631d'
